6ac08fb70fe9684a42c27f61d0b1494d0ddf9554
Flavio Coradini <coradiniflavio@gmail.com>
2021-04-08 18:24:21 +0100

    typo fix

--- tests/unit/validation.test.js
+++ tests/unit/validation.test.js
@@ -1,7 +1,7 @@
 const { validateInputParams } = require("../../src/validation");
 
 describe("validateInputParams", () => {
-  test("input params is required", () => {
+  test("input params are required", () => {
     expect(() => validateInputParams()).toThrow("input params are required");
   });
 
