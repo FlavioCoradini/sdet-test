6f827c289b9011c0a374f5d1fa44f5fb61f1127d
Flavio Coradini <coradiniflavio@gmail.com>
2021-04-08 20:46:35 +0100

    removing tests

--- tests/integration/compute.test.js
+++ tests/integration/compute.test.js
@@ -11,29 +11,4 @@ describe("compute", () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty("probability");
   });
-
-  test("compute should return 400 if area_code is missing", async () => {
-    const res = await request(app).post("/compute").send({
-      sample: "mocked sample",
-    });
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("message");
-  });
-
-  test("compute should return 400 if sample is missing", async () => {
-    const res = await request(app).post("/compute").send({
-      area_code: 1,
-    });
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("message");
-  });
-
-  test("compute should return 400 if params are missing", async () => {
-    const res = await request(app).post("/compute").send();
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("message");
-  });
 });
--- tests/unit/validation.test.js
+++ tests/unit/validation.test.js
@@ -4,20 +4,4 @@ describe("validateInputParams", () => {
   test("input params are required", () => {
     expect(() => validateInputParams()).toThrow("input params are required");
   });
-
-  test("area_code is required", () => {
-    const body = {
-      sample: 11211,
-    };
-
-    expect(() => validateInputParams(body)).toThrow("area_code is required");
-  });
-
-  test("sample is required", () => {
-    const body = {
-      area_code: "mocked-area-code",
-    };
-
-    expect(() => validateInputParams(body)).toThrow("sample is required");
-  });
 });
