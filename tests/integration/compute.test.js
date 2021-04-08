5ae04ff3d5458aa83c206eb94f9ebf11a13ff9af
Flavio Coradini <coradiniflavio@gmail.com>
2021-04-08 19:26:03 +0100

    integration test added

--- tests/integration/compute.test.js
+++ tests/integration/compute.test.js
@@ -2,11 +2,38 @@ const request = require("supertest");
 const app = require("../../src/app");
 
 describe("compute", () => {
-  test("compute should return 200", () => {
-    expect(true).toBe(true);
+  test("compute should return 200", async () => {
+    const res = await request(app).post("/compute").send({
+      area_code: 1,
+      sample: "mocked sample",
+    });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("probability");
   });
 
-  test("compute should return 400", () => {
-    expect(true).toBe(true);
+  test("compute should return 400 if area_code is missing", async () => {
+    const res = await request(app).post("/compute").send({
+      sample: "mocked sample",
+    });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("message");
+  });
+
+  test("compute should return 400 if sample is missing", async () => {
+    const res = await request(app).post("/compute").send({
+      area_code: 1,
+    });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("message");
+  });
+
+  test("compute should return 400 if params are missing", async () => {
+    const res = await request(app).post("/compute").send();
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("message");
   });
 });
