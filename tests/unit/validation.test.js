f6efbcd32a7b9081eb4065c2496cd06f20dae34f
Flavio Coradini <coradiniflavio@gmail.com>
2021-04-08 18:23:59 +0100

    unit tests added

---
+++ tests/unit/validation.test.js
@@ -0,0 +1,23 @@
+const { validateInputParams } = require("../../src/validation");
+
+describe("validateInputParams", () => {
+  test("input params is required", () => {
+    expect(() => validateInputParams()).toThrow("input params are required");
+  });
+
+  test("area_code is required", () => {
+    const body = {
+      sample: 11211,
+    };
+
+    expect(() => validateInputParams(body)).toThrow("area_code is required");
+  });
+
+  test("sample is required", () => {
+    const body = {
+      area_code: "mocked-area-code",
+    };
+
+    expect(() => validateInputParams(body)).toThrow("sample is required");
+  });
+});
